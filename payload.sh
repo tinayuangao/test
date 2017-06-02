fb0d9e29371c9613926a29581580afce1a690eff
Yuan Gao <tinagao@google.com>
2017-06-02 16:32:04 -0700

    .

--- payload.sh
+++ payload.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+
 PROJECT_NAME='tina-material-tree'
 date=`date +%Y-%m-%d`
 payloadData='{special: $TRAVIS_COMMIT}'
 TOKEN=$FIREBASE_TOKEN
 
+echo $date
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
   firebase database:update --data "$payloadData" --project $PROJECT_NAME --confirm --token "$TOKEN" /payload/$date/$TRAVIS_COMMIT
 fi
